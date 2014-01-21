3c73e2f491ee8ff815f827ed73a65d62413b36ac
Alexander Kuzmin <roosit@abricos.org>
2014-01-21 09:34:52 +0400

    v0.2.4-dev

--- module.php
+++ module.php
@@ -24,7 +24,7 @@ class FeedbackModule extends Ab_Module {
 	 * Конструктор 
 	 */
 	public function FeedbackModule(){
-		$this->version = "0.2.3.1";
+		$this->version = "0.2.4-dev";
 		$this->name = "feedback";
 		$this->takelink = "feedback";
 		
